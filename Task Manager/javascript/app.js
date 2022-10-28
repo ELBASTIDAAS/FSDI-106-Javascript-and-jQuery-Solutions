271c2104e46f7fdfece30968d1a80c8c8102cffa
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-10-27 19:36:58 -0700

    .

--- Task Manager/javascript/app.js
+++ Task Manager/javascript/app.js
@@ -34,6 +34,33 @@ function saveTask() {
     let contact = $("#txtContact").val();
     let participants = $("#txtParticipants").val();
     let color = $("#selColor").val();
+
+    let task = new Task(isImportant, tittle, description, priority, dueDate, contact, participants, color);
+    console.log(task);
+    display(task);
+}
+
+function display(task) {
+    console.log(task);
+    let syntax = `<div class="task">
+    <div class="head">
+        <h5>${task.tittle}</h5>
+        <p>${task.description}</p>
+    </div>
+
+    <div class ="middle">
+        <label>${task.priority}</label>
+        <label>${task.dueDate}</label>
+    </div>
+
+    <div class="tail">
+        <label>${task.contact}</label>
+        <label>${task.participants}</label>
+    </div>
+
+    </div>`;
+
+    $("#task-list").append(syntax);
 }
 
 function init() {
---
+++ Task Manager/javascript/task.js
@@ -0,0 +1,16 @@
+class Task{
+    constructor(important,tittle, description, priority, dueDate, contact, participants, color){
+        this.important = important;
+        this.tittle = tittle;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.contact = contact;
+        this.participants = participants;
+        this.color = color;
+    }
+
+    test(){
+        console.log("test");
+    }
+}
